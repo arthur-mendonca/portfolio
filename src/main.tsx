f25b051a788986ece8679b5d95967eb0f218a124
Arthur Silva Mendonça <arthu91@gmail.com>
2023-07-27 15:20:20 -0300

    fix: apagados imports não utilizados

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React, { useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { GlobalStyle } from "@/styles/Global";
-import { Home } from "@/pages/home";
 import { NavBar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-import { AboutContext } from "contexts/aboutContext";
 import { Router } from "../src/routes/routes";
 import { BrowserRouter } from "react-router-dom";
 
