d775ffc654696019bdc38300b01604808af2a452
Arthur Silva Mendonça <arthu91@gmail.com>
2023-07-27 15:03:53 -0300

    fix: pasta contexts deletada

--- src/contexts/interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IAboutContext {
-  showAbout: boolean;
-  setShowAbout: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-export interface IAboutProvider {
-  children: React.ReactNode;
-}
